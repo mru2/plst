123e919ab354e0a153e6800b01b39171a1b7dbfb
mru2 <mru2@ip-10-224-83-146.eu-west-1.compute.internal>
2014-01-08 13:25:26 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello.txt', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', Buffer.byteLength(body));
+  res.end(body);
+});
+
+
+app.listen(14001);
+console.log('Listening on port 14001');
