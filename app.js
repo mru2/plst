14e910ead7d0a9500d7a285232903a4e2b12ed71
David Ruyer <david.ruyer@clicrdv.com>
2014-01-08 21:02:33 +0100

    realtime voting woohoo

--- app.js
+++ app.js
@@ -15,15 +15,38 @@ server.listen(port);
 console.log('Listening on port : ' + port);
 
 
+// For bootstraping the app
+var snapshot = function(){
+  return [
+    {id: 123, title: "Fade Away", artist: "Vitalic", score: 4},
+    {id: 456, title: "I Love It", artist: "Hilltop Hoods ft. Sia", score: 9},
+    {id: 789, title: "Knights of Cydonia (Gramatik Remix)", artist: "Muse", score: 6}
+  ]
+}
+
+
 // Socket handlers
 io.sockets.on('connection', function (socket) {
 
-  socket.emit('connected', { hello: 'world' });
+  // App bootstraping
+  var bootstrapData = snapshot();
+  console.log('SOCKET : bootstraping app with', bootstrapData);
+  socket.emit('connected', bootstrapData);
 
-  socket.on('ping', function (data) {
-    console.log('Socket pinged with msg : ', data);
+  // Voting on a track
+  socket.on('vote', function(trackId){
 
-    // Dispatching the message, because why not
-    socket.emit('ping', data);
+    console.log('SOCKET : receiving vote for', trackId);
+
+    // Handle voting and/or adding to playlist
+    io.sockets.emit('push', {trackId: trackId});
+
   });
+
+  // socket.on('ping', function (data) {
+  //   console.log('Socket pinged with msg : ', data);
+
+  //   // Dispatching the message, because why not
+  //   socket.emit('ping', data);
+  // });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 var app = angular.module('app', [
-  // 'ngAnimate'
+  'ngAnimate'
 
 // ==============
 // Socket service
@@ -41,21 +41,39 @@ var app = angular.module('app', [
 
   console.log('in MainCtrl');
 
-  $scope.messages = [];
-  
+  // To put in a service, to handle scoring, indexing, updates, ...
+  // https://github.com/tomkuk/angular-collection
+  var getTrack = function(trackId){
+    return _.find($scope.playlist, function(track){ return (track.id === trackId); });
+  }
+
+  // Bootstraping the playlist
   socket.on('connected', function(data){
     console.log('Socket connected', data);
-    $scope.ping();
+    $scope.playlist = data;
   });
 
-  socket.on('ping', function(data){
-    console.log('Socket pinged with', data);
-    $scope.messages.push(data);
-  });
-
-  $scope.ping = function(){
-    socket.emit('ping', 'pong');
+  // Voting on a track
+  $scope.vote = function(track){
+    console.log('voting for', track);
+    socket.emit('vote', track.id);
   };
+
+  // Handling updates
+  socket.on('push', function(trackId){
+    console.log('voting for track', trackId.trackId);
+    var track = getTrack(trackId.trackId);
+    track.score += 1;
+  });
+
+  // socket.on('ping', function(data){
+  //   console.log('Socket pinged with', data);
+  //   $scope.messages.push(data);
+  // });
+
+  // $scope.ping = function(){
+  //   socket.emit('ping', 'pong');
+  // };
 
 });
 
